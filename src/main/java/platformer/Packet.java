1ff54b408a80c6fe0f6b2369aa35ce2c0779af24
mybedisred <spencer7230@gmail.com>
2026-04-13 16:07:52 +0000

    packet update

--- src/main/java/platformer/Packet.java
+++ src/main/java/platformer/Packet.java
@@ -6,15 +6,34 @@ import platformer.code.gameengine.graphics.Camera;
 import platformer.code.gamelogic.player.Player;
 
 public class Packet implements Serializable {
-
     
-    private String address;
-    private Player theirPlayer;
-    private Camera cam;
-    private ArrayList<String> keys;
+    private int playerId;
+    private float x;
+    private float y;
+    private boolean isIt;
     
-    
-    public Packet() {
-
+    public Packet(int playerId, float x, float y, boolean isIt){
+        this.playerId = playerId;
+        this.x = x;
+        this.y = y;
+        this.isIt = isIt;
     }
+    
+    public int getPlayerId(){
+        return playerId;
+    }
+    public float getX(){
+        return x;
+    }
+    public float getY(){
+        return y;
+    }
+    public boolean isIt(){
+        return isIt;
+    }
+    
+    public void setIt(boolean isIt){
+        this.isIt = isIt;
+    }
+    
 }
